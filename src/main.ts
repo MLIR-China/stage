68e9453371cb006ffa1a2e5445743f5166abcc23
tsai <jackalcooper@gmail.com>
2022-07-09 20:01:45 +0800

    try build

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,35 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
+import * as os from 'os'
 
+function cmakeArgs(): string[] {
+  return [
+    '-DLLVM_ENABLE_PROJECTS=mlir',
+    '-DLLVM_TARGETS_TO_BUILD=host',
+    '-DCMAKE_C_COMPILER_LAUNCHER=ccache',
+    '-DCMAKE_CXX_COMPILER_LAUNCHER=ccache',
+    '-DLLVM_ENABLE_ASSERTIONS=ON',
+    '-DCMAKE_BUILD_TYPE=Release',
+    '-DLLVM_ENABLE_OCAMLDOC=OFF',
+    '-DLLVM_ENABLE_BINDINGS=OFF'
+  ]
+}
+
+function prefixArgs(): string[] {
+  const homedir = os.homedir()
+  return [`-DCMAKE_INSTALL_PREFIX=${homedir}/llvm-install`]
+}
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const buildDir = 'build'
+    const llvmSrc: string = core.getInput('llvm-dir')
+    await exec.exec(
+      'cmake',
+      ['-S', llvmSrc, '-B', buildDir, '-G', 'Ninja']
+        .concat(cmakeArgs())
+        .concat(prefixArgs())
+    )
+    await exec.exec('cmake', ['--build', buildDir, '-t', 'install'])
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
