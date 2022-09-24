86b03eecc017b1db23751f93e39359300427024b
Shenghang Tsai <jackalcooper@gmail.com>
2022-09-24 18:33:15 +0800

    Try build (#12)
    * try build
    
    * mv llvm checkout down
    
    * rm useless
    
    * add concurrency
    
    * fix
    
    * fix
    
    * use main branch
    
    * fix path
    
    * add ninja
    
    * add ccache
    
    * add -y
    
    * use apt
    
    * add sudo
    
    * add zip and upload
    
    * add tag
    
    * try fix
    
    * try filecheck
    
    * update
    
    * turn off terminfo
    
    * add auto release
    
    * rm branch
    
    * fix try-build
    
    * better names
    
    * rm debug code

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,42 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as exec from '@actions/exec'
+import * as os from 'os'
+import path from 'path'
+function cmakeArgs(): string[] {
+  return [
+    '-DLLVM_ENABLE_PROJECTS=mlir',
+    '-DLLVM_TARGETS_TO_BUILD=host',
+    '-DCMAKE_C_COMPILER_LAUNCHER=ccache',
+    '-DCMAKE_CXX_COMPILER_LAUNCHER=ccache',
+    '-DLLVM_ENABLE_ASSERTIONS=ON',
+    '-DCMAKE_BUILD_TYPE=Release',
+    '-DLLVM_ENABLE_OCAMLDOC=OFF',
+    '-DLLVM_ENABLE_BINDINGS=OFF',
+    '-DLLVM_INSTALL_UTILS=ON',
+    '-DLLVM_ENABLE_ZLIB=OFF',
+    '-DLLVM_ENABLE_ZSTD=OFF',
+    '-DLLVM_ENABLE_TERMINFO=OFF'
+  ]
+}
 
+function prefixArgs(): string[] {
+  const homedir = os.homedir()
+  return [`-DCMAKE_INSTALL_PREFIX=${homedir}/llvm-install`]
+}
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const buildDir = 'build'
+    let llvmSrc: string = core.getInput('llvm-project-root-dir', {
+      required: true
+    })
+    llvmSrc = path.join(llvmSrc, 'llvm')
+    await exec.exec(
+      'cmake',
+      ['-S', llvmSrc, '-B', buildDir, '-G', 'Ninja']
+        .concat(cmakeArgs())
+        .concat(prefixArgs())
+    )
+    await exec.exec('cmake', ['--build', buildDir, '-t', 'install'])
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
